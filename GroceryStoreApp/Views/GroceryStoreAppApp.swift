e879157775423cf7addfc3b7916997b155168c6f
Florian Kasperbauer <20kasperbauefr2@gmail.com>
2024-12-05 17:46:07 -0500

    Made base classes and some base view/viewmodels
    for Item,Store,
    StoreListView , StoreListViewModel

---
+++ GroceryStoreApp/Views/GroceryStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreAppApp.swift
+//  GroceryStoreApp
+//
+//  Created by Flrorian Kasperbauer on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
