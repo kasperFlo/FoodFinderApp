897cae999acc09ea832b0b8ffca1d1c4615dc463
sutha06 <suthakaran.siva0601@gmail.com>
2024-12-05 20:57:35 -0500

    MainTabView View Made

--- GroceryStoreApp/Views/Interaction/MainTabView.swift
+++ GroceryStoreApp/Views/Interaction/MainTabView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "heart.fill")
+                }
+        }
+        .accentColor(.green)
     }
 }
 
